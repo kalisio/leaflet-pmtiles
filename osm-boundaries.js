0a5016d9dfc1e0c75c56f27b7b05356da74e81ce
AdrienKN <125285400+AdrienKN@users.noreply.github.com>
2024-05-23 15:00:06 +0000

    Deploying to gh-pages from @ kalisio/leaflet-pmtiles@9ed2e7c4d2b002fe6531d1f36a823b78f6fb011f 🚀

--- osm-boundaries.js
+++ osm-boundaries.js
@@ -1,15 +1,14 @@
 async function run () {
   const map = L.map('map', {
-    center: [47, 2],
-    zoom: 6,
+    center: [0, 6.8],
+    zoom: 3,
     zoomControl: false,
     fullscreenControl: true
   })
 
-  var selectedLevel = 0
   function dynamicColor(z, f) {
-    if (f.props.admin_level == selectedLevel) return "yellow"
-     else {
+    if (f.props.admin_level == document.getElementById("selectedLevel").value) return "yellow"
+    else {
       if (f.props.admin_level == 2) return "black"
       if (f.props.admin_level == 3) return "orange"
       if (f.props.admin_level == 4) return "rgba(255, 0, 0, 1)"
@@ -17,13 +16,14 @@ async function run () {
       if (f.props.admin_level == 6) return "green"
       if (f.props.admin_level == 7) return "rgba(255, 196, 51, 153)"
       if (f.props.admin_level == 8) return "rgba(0, 0, 255, 1)"
-      else return "black" }
+      else return "black"
+    }
   }
 
   function dynamicWidth(z, f) {
-    if (f.props.admin_level < 4) return 3
-    if (f.props.admin_level >=4 && f.props.admin_level <6) return 2
-    else return 1
+    if (f.props.admin_level < 4) return 6
+    if (f.props.admin_level >=4 && f.props.admin_level <6) return 5
+    else return 4
   }
 
   let paintRules = []
@@ -32,11 +32,20 @@ async function run () {
       {
         dataLayer: "level"+adminLevel,
         symbolizer: new protomapsL.LineSymbolizer({
-          color: dynamicColor,
-          width: dynamicWidth,
+          color: "black",
+          width: 6,
           opacity: 1,
-          // dash: [2, 4],
-          //dashColor: dynamicColor
+        })
+      },
+      {
+        dataLayer: "level"+adminLevel,
+        symbolizer: new protomapsL.LineSymbolizer({
+          opacity: 1,
+          width: 4,
+          dashWidth: 2,
+          color: dynamicColor,
+          dashColor: dynamicColor,
+          dash: [2, 4],
         })
       }
     )
@@ -56,13 +65,15 @@ async function run () {
       }
     )
   }
-  console.log(labelRules)
 
-  protomapsL.leafletLayer({
+  let layer = protomapsL.leafletLayer({
     url:'http://127.0.0.1:8081/osm-boundaries-africa.pmtiles',
     paintRules,
-    labelRules
+    labelRules,
   }).addTo(map)
+
+
+  document.getElementById("selectedLevel").addEventListener("click", function(){ layer.redraw()})
   }
 
 run()
