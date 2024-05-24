6f9c28c59902625250c152bb68910947b74da818
AdrienKN <125285400+AdrienKN@users.noreply.github.com>
2024-05-24 15:17:13 +0000

    Deploying to gh-pages from @ kalisio/leaflet-pmtiles@06f624dc987fc2bded616e48ebc18e7e73f5323b 🚀

--- contourlines.js
+++ contourlines.js
@@ -1,16 +1,29 @@
 async function run () {
+  
+  // define map
   const map = L.map('map', {
     center: [42.725385312747186, 1.833743432010808],
     zoom: 10,
     zoomControl: false,
     fullscreenControl: true
   })
-
+  
+  // define function for a dynamic color rendering according entered altitude
   function dynamicColor(z, f) {
-    if (document.getElementById("selectedAltitude").value != "" && f.props.height >= document.getElementById("selectedAltitude").value) return "red"
+    if (document.getElementById("minimumAltitude").value != "" && 
+      document.getElementById("maximumAltitude").value != "" && 
+      f.props.height >= document.getElementById("minimumAltitude").value && 
+      f.props.height <= document.getElementById("maximumAltitude").value) return "red"
+    if (document.getElementById("minimumAltitude").value != "" && 
+      document.getElementById("maximumAltitude").value == "" && 
+      f.props.height >= document.getElementById("minimumAltitude").value) return "red"
+    if (document.getElementById("minimumAltitude").value == "" && 
+      document.getElementById("maximumAltitude").value != "" && 
+      f.props.height <= document.getElementById("maximumAltitude").value) return "red"
     else return "black"
   }
 
+  // define style rules for contourlines
   let paintRules = [
     {
       dataLayer: "contour",
@@ -19,38 +32,44 @@ async function run () {
         width: 1.1,
         opacity: 0.4,
       }),
-      filter: (z,f) => { return f.props.height > 0 && f.props.nth_line in [5, 10] == true }
+      filter: (z,f) => { return f.props.height > 0 && f.props.nth_line in [5, 10] == true}
     },
     {
       dataLayer: "contour",
       symbolizer: new protomapsL.LineSymbolizer({
         color: dynamicColor,
         width: 0.6,
-        opacity: 0.3,        
+        opacity: 0.3,
       }),
       filter: (z,f) => { return f.props.height > 0 && f.props.nth_line in [5, 10] == false}
     },
     ]
 
+  // define style rules for labels
   let labelRules = [
     {
       dataLayer: "contour",
       symbolizer: new protomapsL.LineLabelSymbolizer({
-          labelProps:["height"],
+        labelProps:["height"],
         }),
       filter: (z,f) => { return f.props.nth_line in [5, 10] == true}
     }
   ]
 
-  console.log(labelRules)
-
+  // display the layer by curling the pmtiles file and fitting it with the stylesheet
   let layer = protomapsL.leafletLayer({
     url:'http://127.0.0.1:8081/contourlines-france.pmtiles',
     paintRules,
     labelRules,
   }).addTo(map)
 
-  document.getElementById("selectedAltitude").addEventListener('keyup', function(e){
+  // refresh the page when new data is entered
+  document.getElementById("minimumAltitude").addEventListener('keyup', function(e){
+    if(e.key == 'Enter')
+    layer.redraw();
+  })
+
+  document.getElementById("maximumAltitude").addEventListener('keyup', function(e){
     if(e.key == 'Enter')
     layer.redraw();
   })
